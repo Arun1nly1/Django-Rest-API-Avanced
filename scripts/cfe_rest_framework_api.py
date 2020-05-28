520ba3591e10414d7106361a3592b1af196453f2
Arun Chapagain <arunsansar123@gmail.com>
2020-05-28 13:25:23 +0545

    Advanced upto image handling

--- scripts/cfe_rest_framework_api.py
+++ scripts/cfe_rest_framework_api.py
@@ -1,7 +1,30 @@
 import json
 import requests
+import os
+
 
 ENDPOINT = "http://127.0.0.1:8000/api/status/"
+image_path = os.path.join(os.getcwd(),"hot.jpg")
+
+
+def do_img(method ='get',data = {}, is_json=True, img_path =None):
+    headers = {}
+    if is_json:
+        headers['content-type'] = 'application/json'
+        data = json.dumps(data)
+    if img_path is not None:
+        with open(img_path,'rb') as image:
+            file_data = {
+                'image' : image
+            }
+            r = requests.request(method,ENDPOINT, data =data ,files = file_data, headers=headers)
+    else:
+        r = requests.request(method,ENDPOINT, data =data ,headers=headers)
+    print (r.text)
+    print (r.status_code)
+    return r
+
+do_img(method ='post', data = {'user':100,"content":""}, is_json=False)
 
 def do(method ='get',data = {}, is_json=True):
     headers = {}
@@ -12,5 +35,4 @@ def do(method ='get',data = {}, is_json=True):
     print (r.text)
     print (r.status_code)
     return r
-
 do(method = 'delete' , data={'id':10})
--- status/models.py
+++ status/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 
 def upload_status_image(instance,filename):
-    return "updates/{user}/{filename}".format(user=instance.user, filename=filename)
+    return "status/{user}/{filename}".format(user=instance.user, filename=filename)
 
 
 class StatusQuerySet(models.QuerySet):
@@ -19,7 +19,7 @@ class StatusManager(models.Manager):
 class Status(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField(null= True , blank =True)
-    image = models.ImageField(upload_to = upload_status_image, null=True, blank = True)
+    image = models.ImageField(upload_to = upload_status_image, null=True, blank = True) #Django storages => AWS S3
     timestamp = models.DateTimeField(auto_now = True)
     updated = models.DateTimeField(auto_now_add = True)
 
